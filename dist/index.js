83e54ef3b6d3c7294535cf5896b3a08540993e18
8130092157 <abhaychauhan3802@gmail.com>
2026-06-13 20:58:25 +0530

    add

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,25 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(require("@actions/core"));
-const name = core.getInput("name");
-console.log(`Hello ${name}, Welcome to custom workflow!`);
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    const token = (0, core_1.getInput)('gh-token');
+    const label = (0, core_1.getInput)('label');
+    const ocktokit = (0, github_1.getOctokit)(token);
+    const pullRequest = github_1.context.payload.pull_request;
+    try {
+        if (!pullRequest) {
+            throw new Error('No pull request found in the context');
+        }
+        await ocktokit.rest.issues.addLabels({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+    }
+    catch (error) {
+        (0, core_1.setFailed)(`Action failed with error: ${error}`);
+    }
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-import * as core from "@actions/core";
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
-const name = core.getInput("name");
-console.log(`Hello ${name}, Welcome to custom workflow!`);
+
+
+async function run(): Promise<void> {
+    const token = getInput('gh-token');
+    const label = getInput('label');
+    
+    const ocktokit=getOctokit(token);
+    const pullRequest = context.payload.pull_request;
+
+    try {
+        if (!pullRequest) {
+            throw new Error('No pull request found in the context');
+        }
+
+        await ocktokit.rest.issues.addLabels({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label],
+        });
+    }
+    catch (error) {
+        setFailed(`Action failed with error: ${error}`);
+    }
+}
+    run();
+
