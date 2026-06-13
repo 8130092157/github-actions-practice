34e708ad06c4197ea09ac7728eab8a15ab73d017
8130092157 <abhaychauhan3802@gmail.com>
2026-06-13 21:07:42 +0530

    convert to ESM

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@actions/core");
-const github_1 = require("@actions/github");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 async function run() {
-    const token = (0, core_1.getInput)('gh-token');
-    const label = (0, core_1.getInput)('label');
-    const ocktokit = (0, github_1.getOctokit)(token);
-    const pullRequest = github_1.context.payload.pull_request;
+    const token = getInput('gh-token');
+    const label = getInput('label');
+    const ocktokit = getOctokit(token);
+    const pullRequest = context.payload.pull_request;
     try {
         if (!pullRequest) {
             throw new Error('No pull request found in the context');
         }
         await ocktokit.rest.issues.addLabels({
-            owner: github_1.context.repo.owner,
-            repo: github_1.context.repo.repo,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
             issue_number: pullRequest.number,
             labels: [label],
         });
     }
     catch (error) {
-        (0, core_1.setFailed)(`Action failed with error: ${error}`);
+        setFailed(`Action failed with error: ${error}`);
     }
 }
 run();
